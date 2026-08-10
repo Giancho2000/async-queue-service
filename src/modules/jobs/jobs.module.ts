75c994ecbf17d632af9b1569ed744ab2442a0262
Giancarlo Reina <giancarloreina125@gmail.com>
2026-08-10 01:33:00 -0500

    feat: worker jobs was created only for test job type

--- src/modules/jobs/jobs.module.ts
+++ src/modules/jobs/jobs.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { JobsController } from './jobs.controller';
 import { JobsService } from './jobs.service';
 import { BullModule } from '@nestjs/bullmq';
+import { JobsProcessor } from './jobs.processor';
 
 @Module({
   imports: [BullModule.registerQueue({ name: 'jobs' })],
   controllers: [JobsController],
-  providers: [JobsService],
+  providers: [JobsService, JobsProcessor],
 })
 export class JobsModule {}
